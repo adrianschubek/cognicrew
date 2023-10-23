e827f3428686ffe42886b04bed76ad919dc61e75
Adrian Schubek <adrian.schubek@gmail.com>
2023-10-23 17:52:43 +0200

    add react-native-paper

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
+import * as React from 'react';
+import { AppRegistry } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import { name as appName } from './app.json';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
+
 
 export default function App() {
   return (
+    <PaperProvider>
     <View style={styles.container}>
       <Text>Alex ist ein wunderschöner Mensch! *wink*</Text>
       <StatusBar style="auto" />
+      <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+    Press me
+  </Button>
     </View>
+    </PaperProvider>
   );
 }
 
