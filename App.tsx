0e09a48d31e538c9fdd9aef65b578111d18cfe93
timo_omit <65171386+timohuisinga@users.noreply.github.com>
2023-10-23 18:08:43 +0200

    add Test how variables work

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { AppRegistry } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
-import { name as appName } from './app.json';
+import { expo } from './app.json';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native-paper';
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <PaperProvider>
     <View style={styles.container}>
-      <Text>Alex ist ein wunderschöner Mensch! *wink*</Text>
+      <Text>{expo.name}, die beste App fürs Lernen!</Text>
       <StatusBar style="auto" />
       <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
     Press me
