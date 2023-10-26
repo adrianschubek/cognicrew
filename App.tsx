e38e8f459a04ddf3a97629b62a01cd106b56c327
Adrian Schubek <adrian.schubek@gmail.com>
2023-10-27 00:57:18 +0200

    test format file with prettier

--- App.tsx
+++ App.tsx
@@ -1,32 +1,37 @@
-import * as React from 'react';
-import { AppRegistry } from 'react-native';
-import { PaperProvider } from 'react-native-paper';
-import { expo } from './app.json';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-paper';
+import * as React from "react";
+import { AppRegistry } from "react-native";
+import { PaperProvider } from "react-native-paper";
+import { expo } from "./app.json";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-paper";
 import { NavigationContainer } from "@react-navigation/native";
 
-
 export default function App() {
-  return ( <NavigationContainer>
-    <PaperProvider>
-      <View style={styles.container}>
-        <Text>{expo.name}, die beste App fürs Lernen!</Text>
-        <StatusBar style="auto" />
-        <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
-          Press me
-        </Button>
-      </View>
-    </PaperProvider></NavigationContainer>
+  return (
+    <NavigationContainer>
+      <PaperProvider>
+        <View style={styles.container}>
+          <Text>{expo.name}, die beste App fürs Lernen!</Text>
+          <StatusBar style="auto" />
+          <Button
+            icon="camera"
+            mode="contained"
+            onPress={() => console.log("Pressed")}
+          >
+            Press me
+          </Button>
+        </View>
+      </PaperProvider>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
