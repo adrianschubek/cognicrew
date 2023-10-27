2588b18d830e486c58b83a839cc505eb6ccd5935
timo_omit <65171386+timohuisinga@users.noreply.github.com>
2023-10-27 23:05:28 +0200

    Navigating to a new screen and back

--- App.tsx
+++ App.tsx
@@ -6,7 +6,10 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Button } from "react-native-paper";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { supabase } from "./supabase";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   supabase.auth.signInWithPassword({
@@ -18,23 +21,56 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <PaperProvider>
-        <View style={styles.container}>
-          <Text>{expo.name}, die beste App xxfürs Lernen!</Text>
-          <StatusBar style="auto" />
-          <Button
-            icon="camera"
-            mode="contained"
-            onPress={() => console.log("Pressed")}
-          >
-            Press me
-          </Button>
-        </View>
-      </PaperProvider>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="ManageFriends" component={ManageFriends} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
+function HomeScreen( {navigation}) {
+  return (
+    <PaperProvider>
+    <View style={styles.container}>
+      <Text>{expo.name}, die beste App fürs Lernen!</Text>
+      <StatusBar style="auto" />
+      <Button
+        icon="account-multiple"
+        mode="contained"
+        onPress={() => {
+          navigation.navigate("ManageFriends")
+          console.log("Home Screen Pressed")
+          }
+        }
+      >
+        Manage your friends!
+      </Button>
+    </View>
+  </PaperProvider>
+  );
+}
+function ManageFriends({navigation}) {
+  return (
+    <PaperProvider>
+    <View style={styles.container}>
+      <Text>Leider hast du keine Freunde. Denk mal darüber nach!</Text>
+      <StatusBar style="auto" />
+      <Button
+        icon="home"
+        mode="contained"
+        onPress={() => {
+          navigation.goBack();
+          console.log("ManageFriends Screen Pressed")
+          }
+        }
+      >
+        Go back to the homescreen
+      </Button>
+    </View>
+  </PaperProvider>
+  );
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
