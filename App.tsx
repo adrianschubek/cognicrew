8eda8cba40e55b6c685cd16cc4cc9b3f87671c52
Adrian Schubek <adrian.schubek@gmail.com>
2023-10-29 15:24:29 +0100

    add logout dialog

--- App.tsx
+++ App.tsx
@@ -49,7 +49,7 @@ export default function App() {
     });
 
   return (
-    <PaperProvider
+    <PaperProvider    
       theme={{
         ...DefaultTheme,
         roundness: 2,
--- screens/AccountSettings.tsx
+++ screens/AccountSettings.tsx
@@ -1,8 +1,60 @@
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import { View } from "react-native";
-import { Avatar, Button, Card, Text } from "react-native-paper";
-import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  Avatar,
+  Button,
+  Card,
+  Dialog,
+  IconButton,
+  Portal,
+  Text,
+  useTheme,
+} from "react-native-paper";
 
 const Account = (props) => <Avatar.Icon {...props} icon="account" />;
+
+const LogoutButton = () => {
+  const theme = useTheme();
+  const [showConfirm, setShowConfirm] = useState(false);
+
+  return (
+    <>
+      <Portal>
+        <Dialog visible={showConfirm} onDismiss={() => setShowConfirm(false)}>
+          <Dialog.Title>Confirm</Dialog.Title>
+          <Dialog.Content>
+            <Text variant="bodyMedium">Are you sure you want to logout?</Text>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={() => setShowConfirm(false)}>Cancel</Button>
+            <Button
+              onPress={() => {
+                setShowConfirm(false);
+                // TODO: supabase.auth.signOut();
+              }}
+            >
+              Logout
+            </Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+      <IconButton
+        mode="outlined"
+        iconColor={theme.colors.error}
+        style={{
+          borderColor: theme.colors.error,
+          marginRight: 10,
+          borderRadius: 10,
+        }}
+        icon="logout"
+        onPress={() => {
+          setShowConfirm(true);
+        }}
+      />
+    </>
+  );
+};
 
 const data = {
   name: "John Doe",
@@ -18,11 +70,17 @@ export default function AccountSettings({ nav }) {
         padding: 10,
       }}
     >
+      <StatusBar style="auto" />
       <Card>
-        <Card.Title title="Account information" left={Account} />
+        <Card.Title
+          title="Account Information"
+          left={Account}
+          right={LogoutButton}
+        />
         <Card.Content>
-          <Text variant="titleLarge">{data.name}</Text>
+          <Text variant="bodyLarge">{data.name}</Text>
           <Text variant="bodyMedium">{data.email}</Text>
+          <Text variant="bodyMedium">Account created on XXXXXXX</Text>
         </Card.Content>
       </Card>
       <Card>
