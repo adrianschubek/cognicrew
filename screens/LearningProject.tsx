df34ddc6d9271d5201efe1415aba4f865813aa34
timo_omit <65171386+timohuisinga@users.noreply.github.com>
2023-10-31 15:51:45 +0100

    Outsourced LearningProjectCategory

--- screens/LearningProject.tsx
+++ screens/LearningProject.tsx
@@ -1,54 +1,34 @@
 import * as React from "react";
 import { StatusBar } from "expo-status-bar";
-import { ImageBackground, StyleSheet, View, Image } from "react-native";
-import { Button, Text, Divider } from "react-native-paper";
-import ImageResizeMode from "react-native/Libraries/Image/ImageResizeMode";
+import { StyleSheet, View} from "react-native";
+import { Button, Text} from "react-native-paper";
 import {
   responsiveHeight,
   responsiveWidth,
   responsiveFontSize,
 } from "react-native-responsive-dimensions";
+import LearningProjectCategory from "../components/learningProject/LearningProjectCategory";
 
 export default function ManageFriends({ navigation }) {
   return (
     <View style={styles.container}>
-      <View style={styles.category}>
-        <Text style={styles.textStyle}> Flashcards </Text>
-        <Image
-          style={styles.imageStyle}
-          resizeMode="contain"
-          source={require("../assets/cards_symbol.png")}
-        />
-      </View>
-      <Divider style={styles.dividerStyle} />
-      <View style={styles.category}>
-        <Text style={styles.textStyle}> Exercises </Text>
-        <Image
-          style={styles.imageStyle}
-          resizeMode="contain"
-          source={require("../assets/completed_task_symbol.png")}
-        />
-      </View>
-      <Divider style={styles.dividerStyle} />
-      <View style={styles.category}>
-        <Text style={styles.textStyle}> Videos </Text>
-        <Image
-          style={styles.imageStyle}
-          resizeMode="contain"
-          source={require("../assets/camera_symbol.png")}
-        />
-      </View>
-      <Divider style={styles.dividerStyle} />
-      <View style={styles.category}>
-        <Text style={styles.textStyle}> Files </Text>
-        <Image
-          style={styles.imageStyle}
-          resizeMode="contain"
-          source={require("../assets/files_symbol.png")}
-        />
-      </View>
-      <Divider style={styles.dividerStyle} />
       <StatusBar style="auto" />
+      <LearningProjectCategory
+      path={require("../assets/cards_symbol.png")}
+      name={"Flashcards"}
+      />
+            <LearningProjectCategory
+      path={require("../assets/completed_task_symbol.png")}
+      name={"Exercises"}
+      />
+            <LearningProjectCategory
+      path={require("../assets/camera_symbol.png")}
+      name={"Videos"}
+      />
+            <LearningProjectCategory
+      path={require("../assets/files_symbol.png")}
+      name={"Files"}
+      />
       <Button
         icon="home"
         mode="contained"
@@ -83,27 +63,5 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "flex-start",
-  },
-  category: {
-    width: responsiveWidth(100),
-    height: responsiveHeight(13),
-    flexDirection: "row",
-    justifyContent: "space-between",
-    //backgroundColor:"red",
-  },
-  imageStyle: {
-    height: responsiveHeight(13),
-    width: responsiveWidth(38),
-    flex: 0,
-    //backgroundColor: "green",
-  },
-  dividerStyle: {
-    width: "100%",
-    marginBottom: responsiveHeight(2),
-  },
-  textStyle: {
-    fontSize: responsiveFontSize(3),
-    textAlignVertical: "center",
-    paddingLeft: responsiveWidth(5),
   },
 });
