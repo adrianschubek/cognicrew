d4ebc239f94c30cae047f7f2d614ca4c07ad0450
timo_omit <65171386+timohuisinga@users.noreply.github.com>
2023-11-02 00:43:58 +0100

    added prop width to accordion section

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import { useState } from "react";
 import { TextInput, Text, Button, IconButton } from "react-native-paper";
 import { expo } from ".././app.json";
@@ -9,6 +8,7 @@ import {
   Image,
   Pressable,
   TouchableOpacity,
+  KeyboardAvoidingView,
 } from "react-native";
 import Modal from "react-native-modal";
 import {
@@ -21,8 +21,8 @@ import Register from "../components/dialogues/Register";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { supabase } from "../supabase";
 export default function Login({ navigation }) {
-  const [text, setText] = React.useState("");
-  const [text2, setText2] = React.useState("");
+  const [text, setText] = useState("");
+  const [text2, setText2] = useState("");
   const [showPasswordForgotten, setShowPasswordForgotten] = useState(false);
   const [showRegister, setShowRegister] = useState(false);
   return (
@@ -45,60 +45,68 @@ export default function Login({ navigation }) {
           }}
         />
       </View>
-      <View style={styles.textContainer}>
-        <Text style={styles.titleText}>Welcome to CogniCrew!</Text>
-        <Text style={styles.baseText}>
-          Your favorite place to supercharge {"\n"}
-          your learning, collaborate with peers, and {"\n"}
-          unlock your full potential. {"\n"}
-        </Text>
-      </View>
-      <View style={styles.loginContainer}>
-        <TextInput
-          style={styles.dataInput}
-          label="Username or E-mail address:"
-          placeholder="Max Mustermann"
-          value={text}
-          onChangeText={(text) => setText(text)}
-        />
-        <TextInput
-          style={[styles.dataInput, { marginTop: responsiveHeight(1) }]}
-          label="Password"
-          placeholder="your password"
-          secureTextEntry={true}
-          value={text2}
-          onChangeText={(text2) => setText2(text2)}
-        />
-        <View
-          style={{ flexDirection: "row", marginTop: responsiveHeight(0.2) }}
-        >
-          <Text>Your first time? You can register</Text>
-          <TouchableOpacity onPress={() => setShowRegister(true)}>
-            <Text> here!</Text>
-          </TouchableOpacity>
+        <View style={styles.textContainer}>
+          <Text style={styles.titleText}>Welcome to CogniCrew!</Text>
+          <Text style={styles.baseText}>
+            Your favorite place to supercharge {"\n"}
+            your learning, collaborate with peers, and {"\n"}
+            unlock your full potential. {"\n"}
+          </Text>
         </View>
-        <TouchableOpacity
-          style={{ alignSelf: "flex-start", marginTop: responsiveHeight(0.5) }}
-          onPress={() => {
-            setShowPasswordForgotten(true);
-          }}
-        >
-          <Text>Password forgotten?</Text>
-        </TouchableOpacity>
-        <Button
-          style={[styles.dataInput, { marginTop: responsiveHeight(1.5) }]}
-          mode="contained"
-          onPress={async () => {
-            const { data } = await supabase.auth.signInWithPassword({
-              email: text,
-              password: text2,
-            });
-            // console.log(data);
-          }}
-        >
-          Login
-        </Button>
-      </View>
+        <View style={styles.loginContainer}>
+          <TextInput
+            style={styles.dataInput}
+            label="Username or E-mail address:"
+            placeholder="Max Mustermann"
+            value={text}
+            onChangeText={(text) => setText(text)}
+          />
+          <TextInput
+            style={[styles.dataInput, { marginTop: responsiveHeight(1) }]}
+            label="Password"
+            placeholder="your password"
+            onSubmitEditing={async () => {
+              const { data } = await supabase.auth.signInWithPassword({
+                email: text,
+                password: text2,
+              });
+            }}
+            secureTextEntry={true}
+            value={text2}
+            onChangeText={(text2) => setText2(text2)}
+          />
+          <View
+            style={{ flexDirection: "row", marginTop: responsiveHeight(0.2) }}
+          >
+            <Text>Your first time? You can register</Text>
+            <TouchableOpacity onPress={() => setShowRegister(true)}>
+              <Text> here!</Text>
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity
+            style={{
+              alignSelf: "flex-start",
+              marginTop: responsiveHeight(0.5),
+            }}
+            onPress={() => {
+              setShowPasswordForgotten(true);
+            }}
+          >
+            <Text>Password forgotten?</Text>
+          </TouchableOpacity>
+          <Button
+            style={[styles.dataInput, { marginTop: responsiveHeight(1.5) }]}
+            mode="contained"
+            onPress={async () => {
+              const { data } = await supabase.auth.signInWithPassword({
+                email: text,
+                password: text2,
+              });
+            }}
+          >
+            Login
+          </Button>
+        </View>
     </SafeAreaView>
   );
 }
