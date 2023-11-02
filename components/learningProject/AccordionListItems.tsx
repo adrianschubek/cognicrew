e4ccbeeeb9f4395dc2848bfea702a2d33e3e548c
timo_omit <65171386+timohuisinga@users.noreply.github.com>
2023-11-02 23:34:20 +0100

    added flashcard editing (backend call excluded)

--- components/learningProject/AccordionListItems.tsx
+++ components/learningProject/AccordionListItems.tsx
@@ -1,15 +1,104 @@
+import { max } from "cypress/types/lodash";
 import * as React from "react";
 import { StyleSheet, View } from "react-native";
-import { Text, List } from "react-native-paper";
+import {
+  Text,
+  List,
+  Divider,
+  useTheme,
+  Card,
+  TextInput,
+} from "react-native-paper";
+import {
+  responsiveHeight,
+  responsiveWidth,
+  responsiveFontSize,
+} from "react-native-responsive-dimensions";
 const accordionListItems = [
-  { title: "What does Sigmund Freud base his main theory on?", id: 1 },
-  { title: "Why can't find love and why doesn't my family love me?", id: 2 },
-  { title: "Why do we live?", id: 3 },
-  { title: "How can i convince my gf to buy me a Porsche?", id: 4 },
+  {
+    question: "What does Sigmund Freud base his main theory on?",
+    answer: "on case studies of his own patients and those of his colleagues",
+    id: 1,
+  },
+  {
+    question: "Why can't I find love and why doesn't my family love me?",
+    answer: "because I am clearly unloveable",
+    id: 2,
+  },
+  {
+    question: "Why do we live?",
+    answer: "we live ultimately to survive and pass on our genes",
+    id: 3,
+  },
+  {
+    question: "How can i convince my gf to buy me a Porsche?",
+    answer:
+      "use of questionable, agressive and emotionaly manipulative tactics",
+    id: 4,
+  },
 ];
 
 export default function AccoridionListItems() {
+  const theme = useTheme();
+  const [question, setQuestion] = React.useState("");
+  const [answer, setAnswer] = React.useState("");
   return accordionListItems.map((listItem) => (
-    <List.Item title={listItem.title} key={listItem.id} />
+    <>
+      <List.Accordion
+        title={listItem.question}
+        key={listItem.id}
+        style={{
+          width: responsiveWidth(100),
+          backgroundColor: theme.colors.secondaryContainer,
+        }}
+        onPress={() => {
+          setQuestion(listItem.question);
+          setAnswer(listItem.answer);
+        }}
+      >
+        <Card elevation={1} style={styles.cardStyle} key={listItem.id}>
+          <Card.Title title="edit here:" />
+          <Card.Content style={styles.cardContentStyle}>
+            <TextInput
+              style={[
+                styles.textInputStyle,
+                { marginBottom: responsiveHeight(1) },
+              ]}
+              multiline={true}
+              label="question:"
+              value={question}
+              onChangeText={(question) => {
+                setQuestion(question);
+                console.log(question);
+                //update backend
+              }}
+            />
+            <TextInput
+              style={styles.textInputStyle}
+              label="answer:"
+              multiline={true}
+              value={answer}
+              onChangeText={(answer) => {
+                setAnswer(answer);
+                console.log(answer);
+                //update backend
+              }}
+            />
+          </Card.Content>
+        </Card>
+      </List.Accordion>
+      <Divider />
+    </>
   ));
 }
+
+const styles = StyleSheet.create({
+  cardStyle: {
+    width: responsiveWidth(100) - responsiveHeight(2),
+    marginTop: responsiveHeight(1),
+    marginBottom: responsiveHeight(1),
+    alignSelf: "center",
+  },
+  cardContentStyle: {},
+  textInputStyle: {},
+});
