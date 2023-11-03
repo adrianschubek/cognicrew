da5a6c506dd3ad7d825f5dfbdd6766d887a1fba1
Adrian Schubek <adrian.schubek@gmail.com>
2023-11-03 15:33:40 +0100

    comment code

--- utils/hooks.ts
+++ utils/hooks.ts
@@ -2,6 +2,12 @@ import { useState, useEffect } from "react";
 import { useAuth } from "../providers/AuthProvider";
 import { getUsername } from "./queries";
 
+/**
+ * Returns the username of the user with the given uid.
+ * If no uid is given, the username of the current user is returned.
+ * @param uid The uid of the user whose username to get.
+ * @returns The username of the user with the given uid.
+ */
 export function useUsername(uid?: string): string {
   const { user } = useAuth();
   const [profilename, setProfilename] = useState("");
