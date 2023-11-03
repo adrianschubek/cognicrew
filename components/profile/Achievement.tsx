496345c6a1830ca55a1d21ba4f83be8728ae130d
timo_omit <65171386+timohuisinga@users.noreply.github.com>
2023-11-03 19:37:57 +0100

    small fix Achievements

--- components/profile/Achievement.tsx
+++ components/profile/Achievement.tsx
@@ -4,25 +4,12 @@ import {
   responsiveWidth,
   responsiveFontSize,
 } from "react-native-responsive-dimensions";
-import { StyleSheet, View, Image } from "react-native"; 
+import { StyleSheet, View, Image } from "react-native";
 import { Text, List, Divider } from "react-native-paper";
 import { useAchievements } from "../../utils/hooks";
 
-const achievementItems = [
-  { id: 1, title: 'Achievement 1', description: 'This is a short description.', imageSource: require('../../assets/des_bedarfs.png') },
-  { id: 2, title: 'Achievement 2', description: 'This is a short description.', imageSource: require('../../assets/des_bedarfs.png') },
-  { id: 3, title: 'Achievement 3', description: 'This is a short description.', imageSource: require('../../assets/des_bedarfs.png') },
-  { id: 4, title: 'Achievement 4', description: 'This is a long description due to testing purposes.', imageSource: require('../../assets/des_bedarfs.png') },
-  { id: 5, title: 'Achievement 5', description: 'This is a short description.', imageSource: require('../../assets/des_bedarfs.png') },
-  { id: 6, title: 'Achievement 6', description: 'This is a short description.', imageSource: require('../../assets/des_bedarfs.png') },
-  { id: 7, title: 'Achievement 7', description: 'This is a short description.', imageSource: require('../../assets/des_bedarfs.png') },
-  { id: 8, title: 'Achievement 8', description: 'This is a short description.', imageSource: require('../../assets/des_bedarfs.png') },
-  { id: 9, title: 'Achievement 9', description: 'This is a short description.', imageSource: require('../../assets/des_bedarfs.png') },
-  { id: 10, title: 'Achievement 10', description: 'This is a short description.', imageSource: require('../../assets/des_bedarfs.png') },
-];
-
 export default function Achievement() {
-  const achievements = useAchievements()
+  const achievements = useAchievements();
   return (
     <List.Section style={styles.achievementStyle}>
       {achievements.map((achievement, index) => (
@@ -33,32 +20,38 @@ export default function Achievement() {
             description={achievement.description}
             left={() => (
               <View style={styles.achievementItem}>
-                <Image source={{uri: 'https://iptk.w101.de/storage/v1/object/public/achievements/' + achievement.icon_name}} style={styles.image} />
+                <Image
+                  source={{
+                    uri:
+                      "https://iptk.w101.de/storage/v1/object/public/achievements/" +
+                      achievement.icon_name,
+                  }}
+                  style={styles.image}
+                />
               </View>
             )}
           />
-          {index < achievementItems.length - 1 && <View style={styles.divider} />}
+          {index < achievements.length && <Divider />}
         </React.Fragment>
       ))}
     </List.Section>
   );
 }
 
-
 const styles = StyleSheet.create({
   achievementStyle: {
     width: responsiveWidth(100),
   },
   achievementItem: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   title: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   divider: {
     height: 1, // You can adjust the height as needed
-    backgroundColor: 'grey', // Adjust the color if needed
+    backgroundColor: "grey", // Adjust the color if needed
   },
   image: {
     marginLeft: responsiveWidth(7),
@@ -66,6 +59,6 @@ const styles = StyleSheet.create({
     height: 70,
     marginRight: 3,
     borderRadius: 35, // Half of the width and height to make it round
-    overflow: 'hidden',
+    overflow: "hidden",
   },
 });
