9f46cd34018b172b3fea815af39c9258a0c01c19
Adrian Schubek <adrian.schubek@gmail.com>
2023-11-09 19:08:57 +0100

    fail

--- screens/LearningProjects.tsx
+++ screens/LearningProjects.tsx
@@ -2,12 +2,15 @@ import * as React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import ProjectGroups from "../components/learningProjects/ProjectGroups";
-import { Tooltip, IconButton } from "react-native-paper";
+import { Tooltip, IconButton,Text } from "react-native-paper";
 import { NAVIGATION } from "../types/common";
 import { useAlerts } from "../utils/hooks";
 import { useEffect } from "react";
 import Discover from "./Discover";
 
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import PagerView from "react-native-pager-view";
+const Tab = createMaterialTopTabNavigator();
 
 export default function LearningProjects({ navigation }) {
   const { info } = useAlerts();
@@ -41,10 +44,10 @@ export default function LearningProjects({ navigation }) {
   }, []);
 
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <ProjectGroups/>
-    </View>
+    // <View style={styles.container}>
+    //   <StatusBar style="auto" />
+    //   <ProjectGroups/>
+    // </View>
     // <Tab.Navigator initialRouteName={"default_my_projects"}>
     //   <Tab.Screen name={NAVIGATION.DISCOVER} component={Discover} />
     //   <Tab.Screen
@@ -53,18 +56,29 @@ export default function LearningProjects({ navigation }) {
     //     options={{ title: "My projects" }}
     //   />
     // </Tab.Navigator>
+    <PagerView style={styles.pagerView} initialPage={0}>
+    <View key="1">
+      <Text>First page</Text>
+    </View>
+    <View key="2">
+      <Text>Second page</Text>
+    </View>
+  </PagerView>
   );
 }
 
-// FIXED use other package instead of material top tabs
-// apk crashes on pojects tab!! 
-// problem, is tob tab navigator, not animated
-// @react-navigation/material-top-tabs package is broken!
+// package:  react-native-pager-view
+// crash: Invariant Violation: requireNativeComponent: "RNCViewPager" was not found in the UIManager.
+// apk crashes on projects tab
+// problem, is top tab navigator, not reanimated
+// @react-navigation/material-top-tabs package is broken
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  }, pagerView: {
+    flex: 1,
   },
 });
