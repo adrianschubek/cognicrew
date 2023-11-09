f7f986e19f73d59b5fa23209612f923694c67a31
Adrian Schubek <adrian.schubek@gmail.com>
2023-11-09 01:50:31 +0100

    add secret

--- screens/Discover.tsx
+++ screens/Discover.tsx
@@ -1,10 +1,52 @@
+import { useEffect } from "react";
 import { Text } from "react-native-paper";
-import Animated from "react-native-reanimated";
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat, withTiming
+} from "react-native-reanimated";
+
+const duration = 2000;
+const easing = Easing.inOut(Easing.ease);
 
 export default function Discover() {
+  const sv = useSharedValue(0);
+
+  useEffect(() => {
+    sv.value = withRepeat(
+      withTiming(10, {
+        duration,
+        easing,
+      }),
+      -1,
+    );
+  }, []);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [
+      { rotateZ: sv.value + "deg" },
+      { scaleX: sv.value / 10 },
+      { scaleY: sv.value / 10 },
+    ],
+  }));
+
   return (
-    <Animated.View>
-      <Text>{"chupapi muñaño ".repeat(500)}</Text>
+    <Animated.View
+      style={{
+        ...animatedStyle,
+        display: "flex",
+        alignContent: "center",
+        alignSelf: "center",
+        alignItems: "center",
+        height: "100%",
+        width: "100%",
+        justifyContent: "center",
+      }}
+    >
+      <Text style={{ fontSize: 32, textAlign: "center", marginTop: "65%" }}>
+        chupapi muñaño
+      </Text>
     </Animated.View>
   );
 }
