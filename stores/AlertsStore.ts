c8bc41148c95568e51a24c4f1812cd3a78e9a16c
Adrian Schubek <adrian.schubek@gmail.com>
2023-11-10 20:10:15 +0100

    fix alert cancel text

--- stores/AlertsStore.ts
+++ stores/AlertsStore.ts
@@ -16,7 +16,7 @@ export const DEFAULT_ALERT: Alert = {
   title: "",
   message: "",
   okText: "OK",
-  cancelText: "Cancel",
+  cancelText: "",
   dismissable: true,
   okAction: () => {},
   cancelAction: () => {},
