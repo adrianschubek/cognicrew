d9207a40bc72a90af55ec3436ea4913945d87613
Adrian Schubek <adrian.schubek@gmail.com>
2023-11-11 19:21:27 +0100

    move achievements and friends

--- screens/AccountManage.tsx
+++ screens/AccountManage.tsx
@@ -4,11 +4,19 @@ import AccountSettings from "./AccountSettings";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import { useEffect } from "react";
 import { LogoutButton } from "../components/settings/AccountInfo";
+import ManageFriends from "./ManageFriends";
+import Achievements from "./Achievements";
 
 const Top = createMaterialTopTabNavigator();
 export default function AccountManage({ navigation }) {
   return (
     <Top.Navigator>
+      <Top.Screen name={NAVIGATION.ACHIEVEMENTS} component={Achievements} />
+      <Top.Screen
+        name={NAVIGATION.MANAGE_FRIENDS}
+        options={{ title: "Friends" }}
+        component={ManageFriends}
+      />
       <Top.Screen name={NAVIGATION.SETTINGS} component={AccountSettings} />
     </Top.Navigator>
   );
