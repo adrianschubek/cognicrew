8100abd87744ab61bac4b437eb26892f7b0fc2d7
gettingXthere <falkenberg-marc44@web.de>
2023-11-12 12:02:53 +0100

    Initial LinkMan. system (adding, editing, sharing)

--- components/learningProject/VideoLinkCards.tsx
+++ components/learningProject/VideoLinkCards.tsx
@@ -1,36 +1,85 @@
-import * as React from "react";
-import { StyleSheet, View, ScrollView } from "react-native";
-import { Text, Card, Avatar, IconButton } from "react-native-paper";
-import {
-  responsiveHeight,
-  responsiveWidth,
-  responsiveFontSize,
-} from "react-native-responsive-dimensions";
-const videos = [
-  { title: "Video über Bären", id: 1 },
-  { title: "Video über Spinnen", id: 2 },
-  { title: "Video über Giraffen", id: 3 },
-  { title: "Video über Elefanten", id: 4 },
-  { title: "Video über Löwen", id: 5 },
-];
-export default function VideoLinkCards() {
-  return videos.map((link) => (
-    <Card elevation={1} style={styles.cardStyle} key={link.id}>
+import * as React from 'react';
+import { useState } from 'react';
+import { Card, IconButton, Text, Menu, Divider } from 'react-native-paper';
+import { Share, Image, StyleSheet, Linking, TouchableOpacity } from 'react-native';
+import { responsiveWidth, responsiveHeight } from 'react-native-responsive-dimensions';
+
+export default function VideoLinkCards({ videos, onDelete, onEdit }) {
+  const [expandedId, setExpandedId] = useState(null);
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const getIconForVideo = (videoURL) => {
+    const youtubePatterns = ['youtube.com', 'youtu.be'];
+    const youtubeIcon = 'https://www.iconpacks.net/icons/2/free-youtube-logo-icon-2431-thumb.png';
+    const defaultIcon = 'https://support.discord.com/hc/user_images/yVOeDzOpxgO8ODSf9bDQ-g.png';
+
+    const isYouTubeURL = youtubePatterns.some(pattern => videoURL.includes(pattern));
+    return isYouTubeURL ? youtubeIcon : defaultIcon;
+  };
+
+  const openVideo = (url) => {
+    if (!url) {
+      console.error("URL is not provided for video.");
+      return;
+    }
+
+    Linking.canOpenURL(url).then(supported => {
+      if (supported) {
+        Linking.openURL(url);
+      } else {
+        console.error("Cannot open URL: ", url);
+      }
+    }).catch(err => {
+      console.error("Error in opening URL: ", err);
+    });
+  };
+  const onShare = async (videoTitle) => {
+    try {
+      await Share.share({
+        message: `Check this out: ${videoTitle}`,
+      });
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  return videos.map((video) => (
+    <Card
+      elevation={1}
+      style={styles.cardStyle}
+      key={video.id}
+      onPress={() => openVideo(video.videoURL)}
+      accessible
+      accessibilityLabel={`Video titled ${video.title}`}
+    >
       <Card.Title
-        title={link.title}
-        subtitle="Dies ist ein Video"
+        title={video.title}
+        subtitle={video.subtitle}
         left={() => (
-          <Avatar.Text
-            style={styles.avatarStyle}
-            size={responsiveFontSize(6)}
-            label="A"
-          />
+          <TouchableOpacity onPress={() => openVideo(video.videoURL)}>
+            <Image source={{ uri: getIconForVideo(video.videoURL) }} style={styles.iconStyle} />
+          </TouchableOpacity>
         )}
-        right={() => <IconButton icon="dots-vertical" onPress={() => {}} />}
+        right={() => (
+          <>
+            <IconButton icon="share" onPress={() => onShare(video.title)} />
+            <Menu
+              visible={menuVisible === video.id}
+              onDismiss={() => setMenuVisible(false)}
+              anchor={<IconButton icon="dots-vertical" onPress={() => setMenuVisible(video.id)} />}>
+              <Menu.Item onPress={() => { onDelete(video.id); setMenuVisible(false); }} title="Delete" />
+              <Divider />
+              <Menu.Item onPress={() => { onEdit(video); setMenuVisible(false); }} title="Edit" />
+            </Menu>
+          </>
+        )}
       />
-      {/*<Card.Content style={styles.cardContentStyle}>
-        <Text> Dies ist ein Video! </Text>
-        </Card.Content>*/}
+      {expandedId === video.id && (
+        <Card.Content>
+          <Text>{typeof video.description === 'string' ? video.description : 'Invalid description'}</Text>
+        </Card.Content>
+      )}
+      <IconButton icon={expandedId === video.id ? 'chevron-up' : 'chevron-down'} onPress={() => setExpandedId(expandedId === video.id ? null : video.id)} />
     </Card>
   ));
 }
@@ -41,8 +90,14 @@ const styles = StyleSheet.create({
     width: responsiveWidth(90),
     marginBottom: responsiveHeight(2),
   },
-  cardContentStyle: {
-    flex: 1,
+  thumbnailStyle: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
   },
-  avatarStyle: {},
+  iconStyle: {
+    width: 30,
+    height: 30,
+    marginRight: 10,
+  },
 });
--- screens/projectManagement/LinkManagement.tsx
+++ screens/projectManagement/LinkManagement.tsx
@@ -1,23 +1,67 @@
-import * as React from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, ScrollView } from "react-native";
-import { Text, Card } from "react-native-paper";
-import {
-  responsiveHeight,
-  responsiveWidth,
-  responsiveFontSize,
-} from "react-native-responsive-dimensions";
-import TextWithPlusButton from "../../components/common/TextWithPlusButton";
-import VideoLinkCards from "../../components/learningProject/VideoLinkCards";
+import * as React from 'react';
+import { View, ScrollView, StyleSheet, StatusBar } from 'react-native';
+import { responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions';
+import TextWithPlusButton from '../../components/common/TextWithPlusButton';
+import VideoLinkCards from '../../components/learningProject/VideoLinkCards';
+import AddVideoLink from '../../components/dialogues/AddVideoLink';
+
 export default function LinkManagement() {
+  const [videos, setVideos] = React.useState([
+    {
+      title: 'Video über Bären',
+      subtitle: 'Dies ist ein Video',
+      description: 'Detailed description of the video here...',
+      id: 1,
+      videoURL: 'https://www.youtube.com/watch?v=17cRUUKcVOo',
+    },
+  ]);
+  const [showVideoLinkDialog, setShowVideoLinkDialog] = React.useState(false);
+  const [editingVideo, setEditingVideo] = React.useState(null);
+
+  const handleAddOrEditVideo = (videoData) => {
+    if (editingVideo) {
+      setVideos(videos.map(video => video.id === editingVideo.id ? { ...videoData, id: video.id } : video));
+    } else {
+      setVideos([...videos, { ...videoData, id: videos.length + 1 }]);
+    }
+    console.log("Updated videos: ", videos);
+
+    setEditingVideo(null);
+    setShowVideoLinkDialog(false);
+  };
+
+  const handleEditVideo = (video) => {
+    setEditingVideo(video);
+    setShowVideoLinkDialog(true);
+  };
+
+  const handleDeleteVideo = (videoId) => {
+    setVideos(videos.filter(video => video.id !== videoId));
+  };
+
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
+      <StatusBar />
+      <AddVideoLink
+        video={editingVideo}
+        showVideoLinkDialog={showVideoLinkDialog}
+        close={() => {
+          setShowVideoLinkDialog(false);
+          setEditingVideo(null);
+        }}
+        onSubmit={handleAddOrEditVideo}
+      />
       <View style={styles.upperContainer}>
-        <TextWithPlusButton text="add new link" function={() => {}} />
+        <TextWithPlusButton
+          text="add new link"
+          function={() => {
+            setEditingVideo(null);
+            setShowVideoLinkDialog(true);
+          }}
+        />
       </View>
       <ScrollView>
-      <VideoLinkCards/>
+        <VideoLinkCards videos={videos} onDelete={handleDeleteVideo} onEdit={handleEditVideo} />
       </ScrollView>
     </View>
   );
@@ -28,15 +72,14 @@ const styles = StyleSheet.create({
     width: responsiveWidth(100),
     height: responsiveHeight(100),
     flex: 1,
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "flex-start",
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
   },
   upperContainer: {
     flex: 0,
     width: responsiveWidth(100),
-    //backgroundColor:"red",
-    flexDirection: "row",
-    justifyContent: "flex-end",
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
   },
 });
