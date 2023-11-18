b8556a3b0457a97d432b549672d49441d10f0a6e
Yoojin Kim <yoojin.kim@stud.tu-darmstadt.de>
2023-11-18 02:39:44 +0100

    Discover-Basic List

--- screens/Discover.tsx
+++ screens/Discover.tsx
@@ -1,6 +1,66 @@
-import { View } from "react-native"
-import {Text} from "react-native-paper";
+import { useQuery } from "@supabase-cache-helpers/postgrest-swr";
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  StatusBar,
+  View,
+  StyleSheet,
+  FlatList,
+} from "react-native";
+import { Text } from "react-native-paper";
+import { supabase } from "../supabase";
+import { styles } from "../components/learningRoom/DrawingStyle";
 
 export default function Discover() {
-  return <View><Text>Test123</Text></View>
+  const { data } = useQuery(
+    supabase
+      .from("learning_projects")
+      .select("id,name,description,is_published,created_at,tags,group,owner_id")
+      .eq("is_published", true),
+    {
+      onSuccess(data, key, config) {
+        // errorAlert(JSON.stringify(data));
+      },
+      onError(err, key, config) {
+        errorAlert({
+          message: err.message,
+        });
+      },
+    },
+  );
+
+  type ItemProps = { title: string };
+  const Item = ({ title }: ItemProps) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: StatusBar.currentHeight || 0,
+    },
+    item: {
+      backgroundColor: "#C6D1DD",
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+    },
+    title: {
+      fontSize: 32,
+    },
+  });
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => <Item title={item.name} />}
+        keyExtractor={(item) => item.id}
+      />
+    </SafeAreaView>
+  );
+}
+function errorAlert(arg0: { message: string }) {
+  throw new Error("Function not implemented.");
 }
