e53e7d1b6db91721c5299e2ed17f6e7b67fc71d9
Alexander Gallus <1alexandergallus1@gmail.com>
2023-11-18 23:13:14 +0100

    Added audio to screens, still time-shifted

--- stores/SoundsStore.ts
+++ stores/SoundsStore.ts
@@ -1,14 +1,48 @@
 import { create } from "zustand";
-
-// CURRENTLY NOT IN USE
+import { Audio } from 'expo-av';
 
 type SoundsStoreType = {
-  sound: any,
-  setSound: (path:any) => void,
+  isLoaded: boolean;
+  isLoaded2: boolean;
+  sound: Audio.Sound | null;
+  loadSound1: (audioSource: any) => void;
+  loadSound2: (audioSource: any) => void;
+  playSound: () => void;
+  stopSound: () => void;
 };
 
 export const useSoundsStore = create<SoundsStoreType>((set, get) => ({
+  isLoaded: false,
+  isLoaded2: false,
   sound: null,
-  setSound:  (sound) => set({ sound }),
-
+  loadSound1: async (audioSource) => {
+    try {
+      console.log("LOADED SOUND 1")
+      const { sound } = await Audio.Sound.createAsync(audioSource);
+      set({ sound, isLoaded: true, isLoaded2: false });
+    } catch (error) {
+      console.error("Error loading sound:", error);
+    }
+  },
+  loadSound2: async (audioSource) => {
+    try {
+      console.log("LOADED SOUND 2")
+      const { sound } = await Audio.Sound.createAsync(audioSource);
+      set({ sound, isLoaded2: true, isLoaded: false });
+    } catch (error) {
+      console.error("Error loading sound:", error);
+    }
+  },
+  playSound: async () => {
+    const { sound, isLoaded, isLoaded2 } = get();
+    if (isLoaded && sound || isLoaded2 && sound) {
+      await sound.playAsync();
+    } 
+  },
+  stopSound: async () => {
+    const { sound, isLoaded, isLoaded2 } = get();
+    if (isLoaded && sound || isLoaded2 && sound) {
+      await sound.stopAsync();
+    } 
+  },
 }));
