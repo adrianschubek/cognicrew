3491e97b7b0304bc1514bb4308fdd5f736c27c4e
timo_omit <65171386+timohuisinga@users.noreply.github.com>
2023-11-24 23:10:12 +0100

    Score Animation first basic animation

--- screens/ingame/EndResults.tsx
+++ screens/ingame/EndResults.tsx
@@ -1,14 +1,21 @@
 import { View } from "react-native";
-import { Button, Text } from "react-native-paper";
+import { Button, Text, useTheme } from "react-native-paper";
 import { PublicRoomState, ScreenState } from "../../functions/rooms";
 import { useAlerts } from "../../utils/hooks";
-import { useEffect } from "react";
-
+import { useEffect, useMemo, useState } from "react";
+import Animated, {
+  useAnimatedStyle,
+  withTiming,
+} from "react-native-reanimated";
+import { useSharedValue } from "react-native-reanimated";
+import { all } from "cypress/types/bluebird";
+import { useFocusEffect } from "@react-navigation/native";
 export default function EndResults({
   route,
 }: {
   route: { params: { roomState: PublicRoomState; user_id: string } };
 }) {
+  const theme = useTheme();
   let self;
   //const roomState = route.params.roomState;
   //const user_id = route.params.user_id;
@@ -20,7 +27,7 @@ export default function EndResults({
   self = {
     id: "10",
     username: "TimoDerBoiii",
-    score: 9000,
+    score: 125,
     currentCorrect: false,
     currentDone: false,
   };
@@ -28,14 +35,14 @@ export default function EndResults({
     {
       id: "1",
       username: "Player 1",
-      score: 10,
+      score: 2,
       currentCorrect: false,
       currentDone: false,
     },
     {
       id: "2",
       username: "Player 2",
-      score: 20,
+      score: 100,
       currentCorrect: false,
       currentDone: false,
     },
@@ -54,9 +61,37 @@ export default function EndResults({
       currentDone: false,
     },
   ];
-  useEffect(() => {
-    console.log(self);
-  }, []);
+  const allPlayers = [...otherPlayers, self];
+  //lowest to highest
+  const sortedPlayers = allPlayers.sort((p1, p2) => p1.score - p2.score);
+  function reSortPlayers(
+    players /*players already have to be sorted ascending by score*/,
+  ) {
+    const result = [];
+    let i = 0;
+    while (i < players.length) {
+      result.push(players[i]);
+      i += 2;
+    }
+    i = players.length - 2;
+    while (i > 0) {
+      result.push(players[i]);
+      i -= 2;
+    }
+    return result;
+  }
+  const reSortedPlayers = useMemo(
+    () => reSortPlayers(sortedPlayers),
+    [route.params.roomState],
+  );
+
+  const highestScore = sortedPlayers[sortedPlayers.length - 1].score * 2;
+  let maxheight = 1;
+  if (highestScore > 250) {
+    maxheight = 250 / highestScore;
+  }
+  useFocusEffect(() => {});
+
   return (
     <View
       style={{
@@ -66,10 +101,67 @@ export default function EndResults({
         alignItems: "center",
       }}
     >
-      <Text variant="headlineSmall" style={{ fontWeight: "bold" }}>
-        Your score: {self.score} {/*"\n \n"*/}
+      <View
+        style={{
+          justifyContent: "flex-end",
+          height: 250,
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            gap: 10,
+            alignItems: "flex-end",
+            //backgroundColor: "red",
+          }}
+        >
+          {reSortedPlayers.map((player) => {
+            const height = useSharedValue(0);
+            useEffect(() => {
+              height.value = withTiming(
+                (player.score < 10 ? 10 : player.score) * 2 * maxheight,
+                {
+                  duration: 1000,
+                },
+              );
+            }, []);
+            const animatedStyles = useAnimatedStyle(() => {
+              return {
+                height: height.value,
+              };
+            });
+            return (
+              <Animated.View
+                key={player.id}
+                style={[
+                  {
+                    width: 60,
+
+                    backgroundColor: theme.colors.primary,
+                    justifyContent: "flex-end",
+                    alignItems: "center",
+                  },
+                  animatedStyles,
+                ]}
+              >
+                <Text
+                  variant="bodyMedium"
+                  style={{ color: theme.colors.background }}
+                >
+                  {player.username}
+                </Text>
+              </Animated.View>
+            );
+          })}
+        </View>
+      </View>
+      {/* <Text variant="headlineSmall" style={{ fontWeight: "bold" }}>
+        Your score: {self.score}
       </Text>
-      <Text variant="bodyMedium" style={{ textAlign: "center", marginBottom: 70 }}>
+      <Text
+        variant="bodyMedium"
+        style={{ textAlign: "center", marginBottom: 70 }}
+      >
         You are more important than the others, {"\n"}thats why your score is
         big and bold
       </Text>
@@ -77,7 +169,7 @@ export default function EndResults({
         <Text variant="titleMedium" key={player.id}>
           {player.username}s score: {player.score} {"\n \n"}
         </Text>
-      ))}
+      ))} */}
     </View>
   );
 }
