e79e38a7dfd183176fec8fc4eba83af57389d5bc
Adrian Schubek <adrian.schubek@gmail.com>
2023-11-26 21:09:52 +0100

    update spec

--- functions/rooms.ts
+++ functions/rooms.ts
@@ -38,11 +38,6 @@ export type PublicRoomState = {
      */
     currentCorrect: boolean | null;
     /**
-     * Whether the user submitted an answer in the current round.
-     * @ deprecated use currentCorrect instead. it implies currentDone
-     */
-    // currentDone: boolean | null;
-    /**
      * Time needed to answer the current question in milliseconds.
      * Will be null if there is no current round, it is disabled or the user did not answer yet.
      */
