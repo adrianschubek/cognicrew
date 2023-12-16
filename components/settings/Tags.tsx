f77ba7f759e807e80b36e5f50717fba2c83ec183
Alexander Gallus <1alexandergallus1@gmail.com>
2023-12-16 15:45:14 +0100

    added interest tags update functionality

--- components/settings/Tags.tsx
+++ components/settings/Tags.tsx
@@ -1,12 +1,33 @@
 import React, { useState } from "react";
 import { Avatar, Button, Card, HelperText, TextInput } from "react-native-paper";
+import { supabase } from "../../supabase";
+import { useAlerts } from "react-native-paper-fastalerts";
+import { useAuth } from "../../providers/AuthProvider";
 
 const Tags = (props) => <Avatar.Icon {...props} icon="tag-multiple" />;
 
 export default function TagsSettings(props) {
-  const [tags, setTags] = useState(null);
+
+  const { success, error: errorAlert } = useAlerts();
+  const [tags, setTags] = useState("");
+  const { user } = useAuth();
 
   const updateTags = async () => {
+    const { data, error } = await supabase
+    .from("profiles")
+    .update(({user_tags: tags }) as { [key: string]: any }) 
+    .eq("id", user.id)
+    .select();
+
+    if (error)
+      errorAlert({
+        message: error.message,
+      });
+    else
+      success({
+        message:
+          "Tags updated successfully.",
+      });
   }
 
   return (
