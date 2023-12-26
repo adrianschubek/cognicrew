b2ba82e4ce349d0a9f945566614a1d82aa8b4bb6
timo_omit <65171386+timohuisinga@users.noreply.github.com>
2023-12-26 03:15:43 +0100

    further changes to Project card

--- components/learningProjects/ProjectCard.tsx
+++ components/learningProjects/ProjectCard.tsx
@@ -1,6 +1,13 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { View } from "react-native";
-import { Button, Card, Icon, Text, useTheme } from "react-native-paper";
+import {
+  Button,
+  Card,
+  Divider,
+  Icon,
+  Text,
+  useTheme,
+} from "react-native-paper";
 import { useAlerts } from "react-native-paper-fastalerts";
 import LearningProjectAvatarWithTitle from "../learningProject/LearningProjectAvatarWithTitle";
 
@@ -23,58 +30,93 @@ export default function ProjectCard(props: {
   ];
   return (
     <Card style={{ margin: 3, marginBottom: 10 }}>
-      <Card.Content
-        style={{ flexDirection: "row", flex: 1, alignItems: "center" }}
-      >
-        <LearningProjectAvatarWithTitle projectName={item.name} />
+      <Card.Content style={{ flexDirection: "column", gap: 10 }}>
+        <View style={{ flexDirection: "row", flex: 1, alignItems: "center" }}>
+          <View style={{ width: "33%" }}>
+            <LearningProjectAvatarWithTitle projectName={item.name} />
+          </View>
+          <View
+            style={{
+              flexDirection: "column",
+              marginLeft: 30,
+              flex: 1,
+            }}
+          >
+            {stats.map((stat, index) => {
+              return (
+                <Fragment key={index}>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      flex: 1,
+                      marginVertical: 2,
+                    }}
+                  >
+                    <View style={{ flex: 0.4 }}>
+                      <Text variant="bodyMedium">{stat.title}: </Text>
+                    </View>
+                    <View
+                      style={{
+                        flex: 0.6,
+                        flexDirection: "row",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Text
+                        variant="bodyMedium"
+                        style={{ fontWeight: "bold", flexShrink: 1 }}
+                      >
+                        {stat.data}
+                      </Text>
+                      {stat.icon && stat.icon}
+                    </View>
+                  </View>
+                  {stat.title !== "Rating" && (
+                    <View style={{ paddingLeft: 0, paddingRight: 10 }}>
+                      <Divider />
+                    </View>
+                  )}
+                </Fragment>
+              );
+            })}
+          </View>
+        </View>
         <View
-          style={{ flexDirection: "column", marginLeft: 30, flex: 1, gap: 5 }}
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
         >
-          {stats.map((stat, index) => {
-            return (
-              <View style={{ flexDirection: "row", flex: 1 }} key={index}>
-                <Text variant="bodyMedium">{stat.title}: </Text>
-                <Text
-                  variant="bodyMedium"
-                  style={{ fontWeight: "bold", flexShrink: 1 }}
-                >
-                  {stat.data}
-                </Text>
-                {stat.icon && stat.icon}
-              </View>
-            );
-          })}
           <Text variant="bodyMedium" style={{ fontStyle: "italic" }}>
             Tags: {item.tags}
           </Text>
+          <Button
+            buttonColor={theme.colors.primary}
+            textColor={theme.colors.onPrimary}
+            onPress={() => {
+              confirm({
+                title: "Clone project",
+                icon: "content-copy",
+                okText: "Clone",
+                okAction: (vars) => {
+                  save(item, vars[0]);
+                },
+                fields: [
+                  {
+                    label: "New project name",
+                    type: "text",
+                    required: true,
+                    errorText: "Cannot clone this project without a new name",
+                  },
+                ],
+              });
+            }}
+          >
+            Clone
+          </Button>
         </View>
       </Card.Content>
-      <Card.Actions style={{ justifyContent: "flex-end" }}>
-        <Button
-          buttonColor={theme.colors.primary}
-          textColor={theme.colors.onPrimary}
-          onPress={() => {
-            confirm({
-              title: "Clone project",
-              icon: "content-copy",
-              okText: "Clone",
-              okAction: (vars) => {
-                save(item, vars[0]);
-              },
-              fields: [
-                {
-                  label: "New project name",
-                  type: "text",
-                  required: true,
-                  errorText: "Cannot clone this project without a new name",
-                },
-              ],
-            });
-          }}
-        >
-          Clone
-        </Button>
-      </Card.Actions>
     </Card>
   );
 }
