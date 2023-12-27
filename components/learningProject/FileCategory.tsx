01da461b4b3388d1e897487312fab03b9288c188
timo_omit <65171386+timohuisinga@users.noreply.github.com>
2023-12-27 22:32:28 +0100

    add HelperText when FileSet is empty

--- components/learningProject/FileCategory.tsx
+++ components/learningProject/FileCategory.tsx
@@ -6,9 +6,10 @@
 import React, { useState } from "react";
 import { View, FlatList } from "react-native";
 import FileItem from "./FileItem";
-import { List, Divider } from "react-native-paper";
+import { List, Divider, HelperText, useTheme } from "react-native-paper";
 
 export default function FileCategory({ title, files }) {
+  const theme = useTheme();
   const [expanded, setExpanded] = useState<boolean>(false);
   return (
     <List.Accordion
@@ -17,6 +18,15 @@ export default function FileCategory({ title, files }) {
       expanded={expanded}
       onPress={() => setExpanded(!expanded)}
     >
+      {!files.length && (
+        <HelperText
+          type="info"
+          style={{ backgroundColor: theme.colors.secondaryContainer }}
+        >
+          There are no files in this set. Add some via the buttons on
+          the bottom right
+        </HelperText>
+      )}
       <View style={{ marginLeft: -40, marginRight: -40 }}>
         <FlatList
           data={files}
