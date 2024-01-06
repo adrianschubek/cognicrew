e766e0856292a297acefa818cd4cd2d8ba532c90
Adrian Schubek <adrian.schubek@gmail.com>
2024-01-07 00:57:53 +0100

    fix date format

--- components/learningProjects/ProjectCard.tsx
+++ components/learningProjects/ProjectCard.tsx
@@ -7,6 +7,7 @@ import { supabase } from "../../supabase";
 import { useNavigation } from "@react-navigation/native";
 import { NAVIGATION } from "../../types/common";
 import { useRoomStore } from "../../stores/RoomStore";
+import dayjs from "dayjs";
 
 export default function ProjectCard(props: {
   item: any;
@@ -24,13 +25,17 @@ export default function ProjectCard(props: {
       title: "Owner",
       data: item.project_owner_name /*+ "xxxxxxxx"*/,
     },
-    { title: "Created in", data: item.project_created_at.substring(0, 4) },
+    {
+      title: "Created",
+      data: dayjs(item.project_created_at).format("DD.MM.YYYY HH:mm"),
+    },
   ];
   const ratingStats = {
     title: "Rating",
     data: item.project_avg_rating.toFixed(1),
     icon: <Icon source="star" size={30} color="#ffb300" />,
   };
+
   return (
     <TouchableOpacity
       onPress={() => {
@@ -154,7 +159,7 @@ export default function ProjectCard(props: {
                         const { data, error } = await supabase.rpc(
                           "create_room",
                           {
-                            p_project_id: parseInt(item.project_id),
+                            p_project_id: item.project_id,
                             p_name: params[0] ?? null,
                             p_code: parseInt(params[1]) ?? null,
                             p_share_code: parseInt(params[2]) ?? null,
