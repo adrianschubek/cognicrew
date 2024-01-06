1fced02c2973a9482c497b236ae28e8243bcd232
timo_omit <65171386+timohuisinga@users.noreply.github.com>
2024-01-06 01:44:24 +0100

    delete Stylesheet stuff in Home.tsx. Inline is better when styles are not reused!

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,18 +1,10 @@
 import * as React from "react";
-import { View, StyleSheet, Image } from "react-native";
+import { View, Image } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import {
-  Text,
-  Card,
-  useTheme,
-} from "react-native-paper";
+import { Text, Card, useTheme } from "react-native-paper";
 import JoinRoom from "../components/learningRoom/JoinRoom";
 import { useUsername } from "../utils/hooks";
 import { useEffect } from "react";
-
-//for testing purposes
-import { NAVIGATION } from "../types/common";
-import { useAvatarStore } from "../stores/AvatarStore";
 import { useAuth } from "../providers/AuthProvider";
 import ProfilePictureAvatar from "../components/profile/ProfilePictureAvatar";
 import LoadingOverlay from "../components/alerts/LoadingOverlay";
@@ -27,7 +19,7 @@ export default function HomeScreen({ navigation }) {
       headerRight: () => (
         <Image
           source={require("../assets/icon.png")}
-          style={styles.headerIcon}
+          style={{ height: 42, width: 41 }}
         />
       ),
     });
@@ -41,22 +33,26 @@ export default function HomeScreen({ navigation }) {
       </>
     );
   return (
-    <View style={styles.container}>
+    <View style={{ flex: 1, justifyContent: "center", paddingHorizontal: 20 }}>
       <StatusBar style="auto" />
-      <View style={styles.header}>
-        <Text style={styles.headerText}>
-          Hello, {isLoading ? "...." : data}
-        </Text>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          padding: 10,
+        }}
+      >
+        <Text variant="titleLarge">Hello, {isLoading ? "...." : data}</Text>
         <ProfilePictureAvatar
-          style={styles.avatar}
           username={isLoading ? "...." : data}
           userId={user.id}
           size={48}
         />
       </View>
-      <View style={styles.body}>
+      <View style={{ flex: 1, justifyContent: "space-around", padding: 10 }}>
         <Card
-          style={styles.card}
+          style={{ padding: 20, marginVertical: 10 }}
           mode="contained"
           theme={{
             colors: { surfaceVariant: theme.colors.secondaryContainer },
@@ -65,55 +61,6 @@ export default function HomeScreen({ navigation }) {
           <JoinRoom navigation={navigation} />
         </Card>
       </View>
-      {/* for testing purposes*/}
-      {/* 
-      <Button
-      style={{marginBottom: 20}}
-        onPress={() => {
-          navigation.navigate(NAVIGATION.END_RESULTS, {
-            roomState: null,
-            user_id: null,
-          });
-        }}
-      >
-        PRESS ME, DADDY!
-      </Button>*/}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    paddingHorizontal: 20,
-  },
-  header: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    padding: 10,
-  },
-  headerText: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  headerIcon: {
-    height: 42,
-    width: 41,
-  },
-  avatar: {},
-  body: {
-    flex: 1,
-    justifyContent: "space-around",
-    padding: 10,
-  },
-  card: {
-    padding: 20,
-    marginVertical: 10,
-  },
-  button: {
-    width: "80%",
-    alignSelf: "center",
-  },
-});
