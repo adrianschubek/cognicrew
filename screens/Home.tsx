0dcc17460de34288ca0cd01a41428b2a3a2e496c
Adrian Schubek <adrian.schubek@gmail.com>
2024-01-11 04:08:17 +0100

    cleanup home

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { View, Image, ScrollView } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import { Button, Text, useTheme } from "react-native-paper";
+import { Text } from "react-native-paper";
 import JoinRoom from "../components/learningRoom/JoinRoom";
 import { useUsername } from "../utils/hooks";
 import { useEffect } from "react";
@@ -9,12 +9,10 @@ import { useAuth } from "../providers/AuthProvider";
 import ProfilePictureAvatar from "../components/profile/ProfilePictureAvatar";
 import LoadingOverlay from "../components/alerts/LoadingOverlay";
 import RoomsList from "./RoomsList";
-import { supabase } from "../supabase";
 
 export default function HomeScreen({ navigation }) {
   const { data, isLoading } = useUsername();
   const { user } = useAuth();
-  const theme = useTheme();
   useEffect(() => {
     navigation.setOptions({
       title: "CogniCrew",
@@ -26,100 +24,6 @@ export default function HomeScreen({ navigation }) {
       ),
     });
   }, []);
-
-  // useEffect(() => {
-  //   // if (!user || !username /* || !alreadyFetchedRealtime */) return;
-  //   // alreadyFetchedRealtime.current = true;
-  //   const realtime = supabase.channel(`xxx`/* , { // TODO: das ist nen satte hiuer global packen useref
-  //     config: { presence: { key: "1" } },
-  //   } */);
-  //   realtime
-  //     .on("presence", { event: "sync" }, () => {
-  //       const newState = realtime.presenceState();
-  //       console.log(
-  //         "syncccc",
-  //         newState,
-  //         // Object.values(newState).flatMap((e: any) => e[0].user_name),
-  //       );
-  //       // setRealtimeState(
-  //       //   Object.values(newState).flatMap((e: any) => e[0].user_name),
-  //       // );
-  //     })
-
-  //     .on("presence", { event: "join" }, ({ key, newPresences }) => {
-  //       console.log("join", key, newPresences);
-  //     })
-  //     .on("presence", { event: "leave" }, ({ key, leftPresences }) => {
-  //       console.log("leave", key, leftPresences);
-  //     })
-  //     // .subscribe();
-  //   .subscribe(async (status) => {
-  //     if (status !== "SUBSCRIBED") {
-  //       return;
-  //     }
-  //     const presenceTrackStatus = await realtime.track({
-  //      a:5555
-  //     });
-  //     console.log(presenceTrackStatus);
-  //   });
-  // }, []);
-
-  // const fuckme = async () => {
-  //   console.log("FFF");
-  //   const realtime = supabase.channel(`xxx`, {
-  //     config: { presence: { key: "1" } },
-  //   });
-  //   realtime
-  //   .on("presence", { event: "sync" }, () => {
-  //     const newState = realtime.presenceState();
-  //     console.log(
-  //       "yyyyyy",
-  //       newState,
-  //       // Object.values(newState).flatMap((e: any) => e[0].user_name),
-  //     );
-  //     // setRealtimeState(
-  //     //   Object.values(newState).flatMap((e: any) => e[0].user_name),
-  //     // );
-  //   }).
-  //   subscribe(async (status) => {
-  //     if (status !== "SUBSCRIBED") {
-  //       return;
-  //     }
-  //     const presenceTrackStatus = await realtime.track({
-  //       a: 1,
-  //     });
-  //     console.log(presenceTrackStatus+ "aaaaaaaaaaa");
-  //   });
-  // };
-
-  // const fuckme2 = async () => {
-  //   console.log("FFF");
-  //   const realtime = supabase.channel(`xxx`, {
-  //     config: { presence: { key: "1" } },
-  //   });
-  //   realtime
-  //   .on("presence", { event: "sync" }, () => {
-  //     const newState = realtime.presenceState();
-  //     console.log(
-  //       "bbb",
-  //       newState,
-  //       // Object.values(newState).flatMap((e: any) => e[0].user_name),
-  //     );
-  //     // setRealtimeState(
-  //     //   Object.values(newState).flatMap((e: any) => e[0].user_name),
-  //     // );
-  //   }).
-  //   subscribe(async (status, err) => {
-  //     if(err) console.log("SUBSCRIPTION ERROR:"+ err);
-  //     if (status !== "SUBSCRIBED") {
-  //       return;
-  //     }
-  //     const presenceTrackStatus = await realtime.track({
-  //       a: 1222222222,
-  //     });
-  //     console.log(presenceTrackStatus+ "bbbbbbbb");
-  //   });
-  // };
 
   if (isLoading)
     return (
@@ -146,8 +50,6 @@ export default function HomeScreen({ navigation }) {
           marginBottom: 10,
         }}
       >
-        {/* <Button onPress={fuckme}>FFFFF</Button> */}
-        {/* <Button onPress={fuckme2}>HHHHH</Button> */}
         <Text variant="titleLarge">Hello, {isLoading ? "...." : data}</Text>
         <ProfilePictureAvatar
           username={isLoading ? "...." : data}
@@ -164,7 +66,6 @@ export default function HomeScreen({ navigation }) {
         <JoinRoom
           style={{
             gap: 5,
-            //alignSelf: "flex-start",
           }}
         />
         <RoomsList />
