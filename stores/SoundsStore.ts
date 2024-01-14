fd4074652402ad90bc929a102684d53c75c6ea42
timo_omit <65171386+timohuisinga@users.noreply.github.com>
2024-01-14 17:51:03 +0100

    sound persists now

--- stores/SoundsStore.ts
+++ stores/SoundsStore.ts
@@ -1,20 +1,30 @@
 import { create } from "zustand";
-
+import { createJSONStorage, persist } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 type SoundsStoreType = {
   inGame: boolean;
-  setInGame: (audioSource: any) => void;
-
   musicVolume: number;
+  loaded: boolean;
+  setInGame: (audioSource: any) => void;
   setMusicVolume: (volume: number) => void;
+  setLoaded: (loaded: boolean) => void;
 };
 
-export const useSoundsStore = create<SoundsStoreType>((set, get) => ({
-
-  inGame: false,
-  setInGame: (inGame) => set({ inGame }),
-
-  musicVolume: 0,
-  setMusicVolume: (volume: number) => set({ musicVolume: volume }),
-  
-}));
+export const useSoundsStore = create<SoundsStoreType>()(
+  persist(
+    (set, get) => ({
+      inGame: false,
+      musicVolume: 0,
+      loaded: false,
+      setInGame: (inGame) => set({ inGame }),
+      setMusicVolume: (volume: number) => set({ musicVolume: volume }),
+      setLoaded: (loaded: boolean) => set({ loaded }),
+    }),
+    {
+      name: "sounds",
+      storage: createJSONStorage(() => AsyncStorage),
+      onRehydrateStorage: () => (state) => state && state.setLoaded(true),
+    },
+  ),
+);
